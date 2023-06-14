91685d2a65bcc05b2c5067300e6a92577acde04c
aniketsinha620 <aniketsinha620@gmail.com>
2023-06-14 18:12:10 +0530

    new version

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,84 @@
-import { useLayoutEffect, useState } from 'react'
-import './App.css'
-
-
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [input, setInput] = useState("")
-  const [arr, setArr] = useState([])
-  const [ans, setAns] = useState(false)
+  const [input, setInput] = useState('');
+  const [description, setDescription] = useState('');
+  const [arr, setArr] = useState([]);
 
+  const handleInput = (e) => {
+    setInput(e.target.value);
+  };
 
-
-  const handleChange = (event) => {
-    setInput(event.target.value)
-    // console.log(input)
-  }
+  const handleDescription = (e) => {
+    setDescription(e.target.value);
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    setAns(true)
-    console.log(input)
-    setArr([...arr, input])
-    setInput("")
-  }
-  const handleClick = (index) => {
-    let newArr =
-      arr.filter((ele, i) => {
-        return i !== index
-      })
-    setArr(newArr)
-  }
+    if (input.trim() === '' || description.trim() === '') {
+      return;
+    }
 
-  const display = arr.map((ele, i) => {
-    return <li>{ele}
-      <button onClick={() => handleClick(i)}>X</button>
+    const newItem = {
+      title: input,
+      description: description,
+    };
+
+    setArr([...arr, newItem]);
+
+    setInput('');
+    setDescription('');
+  };
+
+  const handleClick = (index) => {
+    const newArr = arr.filter((ele, i) => i !== index);
+    setArr(newArr);
+  };
+
+  const display = arr.map((ele, i) => (
+    <li key={i}>
+      <p>{ele.title}</p>
+      <p className='description'>{ele.description}</p>
+      <button className="delete" onClick={() => handleClick(i)}>X</button>
     </li>
-  })
+  ));
 
   return (
-    <div className="form">
-      <h1>Get Things Done!</h1>
-      <form action="" onSubmit={handleSubmit}>
-        <input type="text"
-          value={input}
-          onChange={handleChange} />
-        <button className='btn'>ADD That</button>
-      </form>
-      {ans && <ul>{display}</ul>}
+    <div className="todoContainer">
+      <div className="todo">
+        <div className="task-enter">
+          <form action="" onSubmit={handleSubmit}>
+            <input type="text" placeholder="Title" value={input} onChange={handleInput} />
+            <textarea
+              name=""
+              id=""
+              cols="30"
+              rows="10"
+              placeholder="Description"
+              value={description}
+              onChange={handleDescription}
+            ></textarea>
+            <button className='save'>Save</button>
+          </form>
+        </div>
+        <div className="display">
+          <div className="pending">
+            <div className="heading">
+              <p>Title</p>
+              <p>Description</p>
+              <p>Delete</p>
+            </div>
+            <div className="task-display">
+              {arr.length > 0 && <ul>{display}</ul>}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+
+    
+  );
 }
 
-export default App
+export default App;
